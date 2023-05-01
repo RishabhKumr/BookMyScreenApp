30ac3479267e2336e326f441df74ba656bf54a8f
RishabhKumr <kumarrishabh201@gmail.com>
2023-05-01 22:31:27 +0530

    first commit

---
+++ MovieHandlerMicroservice/src/main/java/com/movie/MovieHandlerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHandlerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHandlerMicroserviceApplication.class, args);
+	}
+
+}
