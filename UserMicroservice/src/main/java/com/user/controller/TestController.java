bd942a8fc42b1265476fee2d3219a582136805d8
RishabhKumr <kumarrishabh201@gmail.com>
2023-05-16 23:14:41 +0530

    APIs created-check swagger-ui for new apis

--- UserMicroservice/src/main/java/com/user/controller/TestController.java
+++ UserMicroservice/src/main/java/com/user/controller/TestController.java
@@ -3,7 +3,8 @@ package com.user.controller;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpEntity;
@@ -15,20 +16,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.user.dto.ResponseHandler;
 import com.user.dto.TicketBookingDTO;
 import com.user.entity.Movie;
 import com.user.entity.Ticket;
 import com.user.service.KafkaProducerServiceImpl;
-
-
-
 
 @RestController
 @CrossOrigin(origins = "*",allowedHeaders="*")
@@ -75,5 +73,55 @@ public class TestController {
 		ResponseEntity<?> status = this.restTemplate.exchange(url, HttpMethod.POST,entity, Movie.class);
 //		kafkaService.setTemp(status.toString());
 		return status;
+	}
+	
+	@GetMapping("/searchbyname/{movieName}")
+	public ResponseEntity<?> searchMovieByName(@PathVariable String movieName)
+	{
+		String url = "http://localhost:8084/api/v1/movie/getMovieByGenre/"+movieName;
+		List<Movie> movieList = this.restTemplate.getForObject(url, List.class);
+	    if(movieList != null) {
+	        CacheControl cacheControl = CacheControl.maxAge(30,TimeUnit.MINUTES);
+	        return ResponseEntity.ok().cacheControl(cacheControl)
+	                .body(ResponseHandler.generateResponse("Successfully fetched Movies", HttpStatus.OK, movieList));
+	    }
+	    return new ResponseEntity<String>("Movie Not Found!",HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/searchbygenre/{genre}")
+	public ResponseEntity<?> searchMovieByGenre(@PathVariable String genre){
+		String url = "http://localhost:8084/api/v1/movie/getMovieByGenre/"+genre;
+		List<Movie> movieList = this.restTemplate.getForObject(url, List.class);
+	    if(movieList != null) {
+	        CacheControl cacheControl = CacheControl.maxAge(30,TimeUnit.MINUTES);
+	        return ResponseEntity.ok().cacheControl(cacheControl)
+	                .body(ResponseHandler.generateResponse("Successfully fetched Movies", HttpStatus.OK, movieList));
+	    }
+	    return new ResponseEntity<String>("Movies Not Found!",HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/searchbyid/{id}")
+	public ResponseEntity<?> searchByMovieId(@PathVariable Long id){
+		String url = "http://localhost:8084/api/v1/movie/getMovieById/"+id;
+		Movie movie = this.restTemplate.getForObject(url, Movie.class);
+	    if(movie != null) {
+	        CacheControl cacheControl = CacheControl.maxAge(30,TimeUnit.MINUTES);
+	        return ResponseEntity.ok().cacheControl(cacheControl)
+	                .body(ResponseHandler.generateResponse("Successfully fetched Movies", HttpStatus.OK, movie));
+	    }
+	    return new ResponseEntity<String>("Movie Not Found!",HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/ticketbyuserid/{id}")
+	public ResponseEntity<?> getTicketByUserId(@PathVariable Long id)
+	{
+		String url = "http://localhost:8085/api/v1/ticket/getticketsbyuserid/"+id;
+		List<Ticket> ticket = this.restTemplate.getForObject(url, List.class);
+		 if(ticket != null) {
+		        CacheControl cacheControl = CacheControl.maxAge(30,TimeUnit.MINUTES);
+		        return ResponseEntity.ok().cacheControl(cacheControl)
+		                .body(ResponseHandler.generateResponse("Successfully fetched Ticket", HttpStatus.OK, ticket));
+		    }
+		    return new ResponseEntity<String>("Ticket Not Found!",HttpStatus.NO_CONTENT);
 	}
 }
