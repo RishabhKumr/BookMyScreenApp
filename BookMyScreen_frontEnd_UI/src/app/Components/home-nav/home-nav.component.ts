976acb3c2ab0062ff67cb9745bae1c3c60840fc1
RishabhKumr <kumarrishabh201@gmail.com>
2023-05-17 23:25:03 +0530

    UI/UX -> Home, HomeNavBar , footer created

--- BookMyScreen_frontEnd_UI/src/app/Components/home-nav/home-nav.component.ts
+++ BookMyScreen_frontEnd_UI/src/app/Components/home-nav/home-nav.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-nav',
   templateUrl: './home-nav.component.html',
   styleUrls: ['./home-nav.component.css']
 })
-export class HomeNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeNavComponent {
+  @Input() city:string="Select Location";
+  constructor(private router:Router){
+  
   }
-
-}
+  ngOnInit(){
+  this.city = sessionStorage.getItem('location');
+  }
+  navigateToLogin(){
+  this.router.navigate(["login"]);
+  }
+  }
--- BookMyScreen_frontEnd_UI/src/app/Components/home/home.component.ts
+++ BookMyScreen_frontEnd_UI/src/app/Components/home/home.component.ts
@@ -1,15 +1,116 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, ViewChild } from '@angular/core';
+import { LocationService } from 'src/app/Services/location.service';
+import { Observable } from 'rxjs';
+import { Location } from 'src/app/Entity/Location'
+import { NgImageSliderComponent } from 'ng-image-slider';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+selector: 'app-home',
+templateUrl: './home.component.html',
+styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+location: Location | undefined;
 
-  constructor() { }
+@Input() city: string = "";
+constructor(private locationService: LocationService) {
+}
 
-  ngOnInit(): void {
-  }
+ngOnInit() {
+this.Repeat();
+const promise = this.locationService.getLocation();
+promise.subscribe((response) => {
+this.location = response as Location;
+console.log(this.location.city);
+this.city = this.location.city;
+sessionStorage.setItem("location", this.city);
+});
+}
+//image Slider by time
+startIndex = 0;
 
+ImageData = ["/assets/img1.jpg", "/assets/img_nature.jpg", "/assets/img_nature_wide.jpg"];
+Repeat() {
+setTimeout(() => {
+this.__FunctionSlide();
+this.Repeat();
+}, 3000);
+}
+
+__FunctionSlide() {
+const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
+
+for (const x of slides) {
+  const y = x as HTMLElement;
+  y.style.display = 'none';
+}
+if (this.startIndex > slides.length - 1) {
+  this.startIndex = 0;
+  const slide = slides[this.startIndex] as HTMLElement;
+  slide.style.display = 'block';
+  this.startIndex++;
+} else {
+
+  const slide = slides[this.startIndex] as HTMLElement;
+  slide.style.display = 'block';
+  this.startIndex++;
+}
+}
+//ng-image-slider
+@ViewChild('nav') slider: NgImageSliderComponent;
+
+imageObject: Array<object> = [{
+image: '/assets/img1.jpg',
+thumbImage: '/assets/img1.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+},
+{
+image: '/assets/img_nature_wide.jpg',
+thumbImage: '/assets/img_nature_wide.jpg',
+alt: 'alt of image',
+title: 'title of image'
+}];
+prevImageClick() {
+this.slider.prev();
+}
+
+nextImageClick() {
+this.slider.next();
+}
 }
